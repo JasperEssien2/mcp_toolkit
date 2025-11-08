bf1d5ec230c0c26e08abc922c7f3f7d9a958bf87
Jahswill Essien <jasperessien2@gmail.com>
2025-11-08 22:53:48 +0100

    made MCPToolInput and MCPToolProperty fields nullable and not-required

--- lib/src/annotations/annotations.dart
+++ lib/src/annotations/annotations.dart
@@ -1,20 +1,20 @@
 import 'package:equatable/equatable.dart';
 
 class MCPToolInput extends Equatable {
-  const MCPToolInput({required this.toolName, required this.toolDescription});
+  const MCPToolInput({this.toolName, this.toolDescription});
 
-  final String toolName;
-  final String toolDescription;
+  final String? toolName;
+  final String? toolDescription;
 
   @override
-  List<Object> get props => [toolName, toolDescription];
+  List<Object?> get props => [toolName, toolDescription];
 }
 
 class MCPToolProperty extends Equatable {
-  const MCPToolProperty({required this.description, this.isRequired = true, this.name});
+  const MCPToolProperty({this.description, this.isRequired, this.name});
 
-  final String description;
-  final bool isRequired;
+  final String? description;
+  final bool? isRequired;
   final String? name;
 
   @override
