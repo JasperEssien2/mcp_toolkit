f96adebb956567bf643b768a4de22fb5c3a0a8da
Jahswill Essien <jasperessien2@gmail.com>
2025-11-08 22:05:24 +0100

    resolved lint

--- example/mcp_toolkit_example.dart
+++ example/mcp_toolkit_example.dart
@@ -1,6 +1,2 @@
-import 'package:mcp_toolkit/mcp_toolkit.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
+// ignore: avoid_empty_blocks
+void main() {}
