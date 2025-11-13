14d8569bd5c941cfcc59f2705fd24e81fe0c76c0
Jahswill Essien <jasperessien2@gmail.com>
2025-11-13 23:40:28 +0100

    introduce getter `jsonType` to hold schema type in string

--- lib/src/models/callable_property_schema.dart
+++ lib/src/models/callable_property_schema.dart
@@ -3,6 +3,8 @@ import 'package:equatable/equatable.dart';
 sealed class CallablePropertySchema extends Equatable {
   const CallablePropertySchema({required this.name, this.description, this.isRequired});
 
+  String get jsonType;
+
   final String name;
   final String? description;
   final bool? isRequired;
@@ -15,30 +17,45 @@ class StringSchema extends CallablePropertySchema {
   const StringSchema({required super.name, super.description, super.isRequired});
 
   const StringSchema.type() : super(name: '');
+
+  @override
+  String get jsonType => 'string';
 }
 
 class BooleanSchema extends CallablePropertySchema {
   const BooleanSchema({required super.name, super.description, super.isRequired});
 
   const BooleanSchema.type() : super(name: '');
+
+  @override
+  String get jsonType => 'boolean';
 }
 
 class NumberSchema extends CallablePropertySchema {
   const NumberSchema({required super.name, super.description, super.isRequired});
 
   const NumberSchema.type() : super(name: '');
+
+  @override
+  String get jsonType => 'number';
 }
 
 class IntSchema extends CallablePropertySchema {
   const IntSchema({required super.name, super.description, super.isRequired});
 
   const IntSchema.type() : super(name: '');
+
+  @override
+  String get jsonType => 'integer';
 }
 
 class ListSchema extends CallablePropertySchema {
   const ListSchema({required super.name, super.description, super.isRequired, required this.type});
 
   final CallablePropertySchema type;
+
+  @override
+  String get jsonType => 'array';
 
   @override
   List<Object?> get props => [...super.props, type];
@@ -50,6 +67,9 @@ class EnumSchema extends CallablePropertySchema {
   final List<String> options;
 
   @override
+  String get jsonType => 'string';
+
+  @override
   List<Object?> get props => [...super.props, options];
 }
 
@@ -59,17 +79,26 @@ class ObjectSchema extends CallablePropertySchema {
   final List<CallablePropertySchema> properties;
 
   @override
+  String get jsonType => 'object';
+
+  @override
   List<Object?> get props => [...super.props, properties];
 }
 
 class NullSchema extends CallablePropertySchema {
   const NullSchema({required super.name, super.description, super.isRequired});
+
+  @override
+  String get jsonType => 'null';
 }
 
 class InvalidSchema extends CallablePropertySchema {
   const InvalidSchema({required super.name, super.description, super.isRequired = false, required this.error});
 
   final String error;
+
+  @override
+  String get jsonType => 'invalid';
 
   @override
   List<Object?> get props => [...super.props, error];
